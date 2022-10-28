4827e3187c9b7958eeca222c5645dc2962c1706f
jate-koh <jate_ben@hotmail.com>
2022-10-28 13:05:32 +0700

    feat: add env and firebase dbs

---
+++ src/dbs/realtime.js
@@ -0,0 +1,9 @@
+import { firebase } from "@react-native-firebase/database";
+import {
+    REACT_APP_REALTIME_URL
+} from '@env'
+
+const reference = firebase
+    .app()
+    .database(REACT_APP_REALTIME_URL)
+    .ref('/');
