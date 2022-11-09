53a320f3a7670a54685b2244ee6337c7989a3bd3
Grapeka <chotwilaiwan@gmail.com>
2022-11-09 19:35:23 +0700

    feat: add receive send chat layouts

---
+++ src/screens/Chat/layouts/Receive.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Receive = (props) => {
+  return <View style={styles.container}>{props.children}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+});
+
+export default Receive;
