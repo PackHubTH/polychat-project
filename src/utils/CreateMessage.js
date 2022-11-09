ad561a46577d17edb989b6ad214debd284ad3960
Grapeka <chotwilaiwan@gmail.com>
2022-11-09 19:42:33 +0700

    feat: add CreateMessage template function

---
+++ src/utils/CreateMessage.js
@@ -0,0 +1,34 @@
+class message {
+  constructor(sender, receiver, text, location, timestamp, messageId, photo) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.text = text;
+    this.location = location;
+    this.timestamp = timestamp;
+    this.messageId = messageId;
+    this.photo = "";
+  }
+}
+
+const CreateMessage = (
+  sender,
+  receiver,
+  text,
+  location,
+  timestamp,
+  messageId,
+  photo
+) => {
+  const newMessage = new message(
+    sender,
+    receiver,
+    text,
+    location,
+    timestamp,
+    messageId,
+    photo
+  );
+  return newMessage;
+};
+
+export default CreateMessage;
