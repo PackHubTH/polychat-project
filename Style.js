b18f64a191195c6be82953aad1178ed28d1beb28
Grapeka <chotwilaiwan@gmail.com>
2022-11-09 19:31:38 +0700

    style: add lightGrey white and lightBlue

--- Style.js
+++ Style.js
@@ -6,6 +6,9 @@ const color = {
   green: "#28C600",
   red: "#FF0000",
   black: "#1F2937",
+  lightGrey: "#8E8E8E",
+  white: "#FFFFFF",
+  lightBlue: "#188FFC",
 };
 
 export { contentLayout, color };
