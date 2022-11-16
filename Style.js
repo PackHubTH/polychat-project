42ac430c06f59e197048a64ad524d939a5eb9bdc
pointies <parinda8855@gmail.com>
2022-11-16 17:31:16 +0700

    style: add grey and edit HEX green, red, lightGrey

--- Style.js
+++ Style.js
@@ -3,10 +3,11 @@ const contentLayout = {
 };
 
 const color = {
-  green: "#28C600",
-  red: "#FF0000",
+  green: "#059669",
+  red: "#EB3434",
   black: "#1F2937",
-  lightGrey: "#8E8E8E",
+  lightGrey: "#D9D9D9",
+  grey: "#8E8E8E",
   white: "#FFFFFF",
   lightBlue: "#188FFC",
 };
