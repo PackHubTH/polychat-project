91df047384195b9af6e381889530bd55c5dc74cb
Grapeka <chotwilaiwan@gmail.com>
2022-12-10 13:28:54 +0700

    test: add found and not found cases

--- src/__tests__/chat/chat.test.js
+++ src/__tests__/chat/chat.test.js
@@ -1,6 +1,12 @@
 import { getAllUserChat } from '../../utils/dbs/AuthDataOperator';
 
-test('get user chat', () => {
-   const userChat = getAllUserChat('1');
-   expect(typeof userChat).toBe('object');
+// get a user chat(s)
+test('get user chat. Found', async () => {
+   const userChat = await getAllUserChat('OxMqlmqrhzWzmoMscd3FeauPIw72');
+   expect(userChat.length >= 1).toBe(true);
+});
+
+test('get user chat. Not found', () => {
+   const userChat = getAllUserChat('dummy');
+   expect(userChat.length >= 1).toBe(false);
 });
