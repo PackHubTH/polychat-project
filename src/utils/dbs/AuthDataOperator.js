d8e5f0021c22dced1c577e0bf86cc8c5c9f87b19
jate-koh <jate_ben@hotmail.com>
2022-12-11 20:25:25 +0700

    style: apply eslint rules to files

--- src/utils/dbs/AuthDataOperator.js
+++ src/utils/dbs/AuthDataOperator.js
@@ -1,14 +1,14 @@
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import {
-   collection,
-   doc,
-   getDoc,
-   addDoc,
-   setDoc,
-   getDocs,
-   query,
-   where,
-   onSnapshot,
+    collection,
+    doc,
+    getDoc,
+    addDoc,
+    setDoc,
+    getDocs,
+    query,
+    where,
+    onSnapshot,
 } from 'firebase/firestore';
 import { updateProfile } from 'firebase/auth';
 
@@ -17,58 +17,58 @@ import { firestoreDb } from './FireStore';
 import { getSticker } from './StorageOperator';
 
 export const createRegisterData = async (
-   user,
-   email,
-   phoneNum,
-   userId,
-   displayName,
-   firstname,
-   lastname
+    user,
+    email,
+    phoneNum,
+    userId,
+    displayName,
+    firstname,
+    lastname
 ) => {
-   try {
-      await updateProfile(user, {
-         displayName: displayName,
-         phoneNumber: phoneNum,
-      });
-      console.log('Register: Updated auth profile');
-   } catch (error) {
-      console.log(error.message);
-      throw new Error(
-         `Register: Failed to update auth profile for ${user.email}`
-      );
-   }
+    try {
+        await updateProfile(user, {
+            displayName: displayName,
+            phoneNumber: phoneNum,
+        });
+        console.log('Register: Updated auth profile');
+    } catch (error) {
+        console.log(error.message);
+        throw new Error(
+            `Register: Failed to update auth profile for ${user.email}`
+        );
+    }
 
-   try {
-      await setDoc(doc(firestoreDb, 'User', userId), {
-         about: '',
-         userId: userId,
-         firstname: firstname,
-         phoneNumber: phoneNum,
-         lastname: lastname,
-         displayName: displayName,
-         email: email,
-         profilePic: '',
-         assistantList: [],
-         chatList: [],
-         emergencyList: [],
-         friendList: [],
-         friendRequest: [],
-      });
-      console.log('Register: Updated profile document in Firestore');
-   } catch (error) {
-      console.log(error.message);
-      throw new Error(
-         `Register: Failed to upload profile document to Firestore`
-      );
-   }
+    try {
+        await setDoc(doc(firestoreDb, 'User', userId), {
+            about: '',
+            userId: userId,
+            firstname: firstname,
+            phoneNumber: phoneNum,
+            lastname: lastname,
+            displayName: displayName,
+            email: email,
+            profilePic: '',
+            assistantList: [],
+            chatList: [],
+            emergencyList: [],
+            friendList: [],
+            friendRequest: [],
+        });
+        console.log('Register: Updated profile document in Firestore');
+    } catch (error) {
+        console.log(error.message);
+        throw new Error(
+            'Register: Failed to upload profile document to Firestore'
+        );
+    }
 };
 
 export const getUserData = async (userId) => {
-   try {
-      const user = await getDoc(doc(firestoreDb, 'User', userId));
+    try {
+        const user = await getDoc(doc(firestoreDb, 'User', userId));
 
-      if (user.exists()) {
-         console.log(`GetUserData: ${userId};
+        if (user.exists()) {
+            console.log(`GetUserData: ${userId};
                 displayName: ${user.data()?.displayName},
                 firstname: ${user.data()?.firstname},
                 lastname: ${user.data()?.lastname},
@@ -76,58 +76,58 @@ export const getUserData = async (userId) => {
                 phoneNumber: ${user.data()?.phoneNumber},
                 status: ${user.data()?.status},
             `);
-         return user.data();
-      } else {
-         console.log(`GetUserData: ${userId} have no data`);
-      }
-   } catch (error) {
-      console.log(error.message);
-      throw new Error(`GetUserData: Failed to get data for ${userId}`);
-   }
+            return user.data();
+        } else {
+            console.log(`GetUserData: ${userId} have no data`);
+        }
+    } catch (error) {
+        console.log(error.message);
+        throw new Error(`GetUserData: Failed to get data for ${userId}`);
+    }
 };
 
 export const getUserChat = async (channelId) => {
-   try {
-      const chat = await getDoc(
-         doc(firestoreDb, 'ChatChannel', channelId.toString())
-      );
-      console.log('chat');
-      console.log(chat.data());
-      if (chat.exists()) {
-         console.log(chat.data());
-         return chat.data();
-      } else {
-         console.log(`GetUserChat: ${channelId} have no data`);
-      }
-   } catch (error) {
-      console.log(error.message);
-      throw new Error(`GetUserChat: Failed to get data for ${channelId}`);
-   }
+    try {
+        const chat = await getDoc(
+            doc(firestoreDb, 'ChatChannel', channelId.toString())
+        );
+        console.log('chat');
+        console.log(chat.data());
+        if (chat.exists()) {
+            console.log(chat.data());
+            return chat.data();
+        } else {
+            console.log(`GetUserChat: ${channelId} have no data`);
+        }
+    } catch (error) {
+        console.log(error.message);
+        throw new Error(`GetUserChat: Failed to get data for ${channelId}`);
+    }
 };
 
 export const getAllUserChat = async (userId) => {
-   const chats = [];
-   const collectionRef = collection(firestoreDb, 'ChatChannel');
-   try {
-      let query1 = query(collectionRef, where('user1', '==', userId));
-      const querySnap1 = await getDocs(query1);
-      querySnap1.forEach((doc) => {
-         chats.push(doc.data());
-      });
+    const chats = [];
+    const collectionRef = collection(firestoreDb, 'ChatChannel');
+    try {
+        let query1 = query(collectionRef, where('user1', '==', userId));
+        const querySnap1 = await getDocs(query1);
+        querySnap1.forEach((doc) => {
+            chats.push(doc.data());
+        });
 
-      let query2 = query(collectionRef, where('user2', '==', userId));
-      const querySnap2 = await getDocs(query2);
-      querySnap2.forEach((doc) => {
-         chats.push(doc.data());
-      });
+        let query2 = query(collectionRef, where('user2', '==', userId));
+        const querySnap2 = await getDocs(query2);
+        querySnap2.forEach((doc) => {
+            chats.push(doc.data());
+        });
 
-      return chats;
-   } catch (error) {
-      console.log(error.message);
-      throw new Error(
-         `GetAllUserChat: Failed to get user's chat for ${userId}`
-      );
-   }
+        return chats;
+    } catch (error) {
+        console.log(error.message);
+        throw new Error(
+            `GetAllUserChat: Failed to get user's chat for ${userId}`
+        );
+    }
 };
 
 // export const _createRegisterData = ( user ,email, phoneNum, userId, displayName, firstname, lastname, picture) => {
