b91b3bdfa5780e4066deb39357d49e6bdce0072e
Grapeka <chotwilaiwan@gmail.com>
2022-12-11 22:12:56 +0700

    feat: add user mock data

--- src/mocks/user.js
+++ src/mocks/user.js
@@ -5,3 +5,48 @@ export const realUser = {
 export const fakeUser = {
    uid: 'fakeUser',
 };
+
+export const users = [
+   {
+      userId: '1',
+      about: 'I am a real',
+      email: 'Test1@gmail.com',
+      firstname: 'Test1',
+      lastname: 'Test1',
+      displayName: 'Test1',
+      phoneNumber: '1234567890',
+      profilePic:
+         'https://firebasestorage.googleapis.com/v0/b/polychat-6523f.appspot.com/o/profile%2Fsamplepicture%2FEllipse%201.png?alt=media&token=0bb937e1-1235-4ad3-8e44-0aee7bf42fdc',
+      assistantList: [],
+      emergencyList: [],
+      chatList: [],
+   },
+   {
+      userId: '2',
+      about: 'I am a real',
+      email: 'Test2@gmail.com',
+      firstname: 'Test2',
+      lastname: 'Test2',
+      displayName: 'Test2',
+      phoneNumber: '1234567890',
+      profilePic:
+         'https://firebasestorage.googleapis.com/v0/b/polychat-6523f.appspot.com/o/profile%2Fsamplepicture%2FEllipse%201.png?alt=media&token=0bb937e1-1235-4ad3-8e44-0aee7bf42fdc',
+      assistantList: [],
+      emergencyList: [],
+      chatList: [],
+   },
+   {
+      userId: '3',
+      about: 'I am a real',
+      email: 'Test3@gmail.com',
+      firstname: 'Test3',
+      lastname: 'Test3',
+      displayName: 'Test3',
+      phoneNumber: '1234567890',
+      profilePic:
+         'https://firebasestorage.googleapis.com/v0/b/polychat-6523f.appspot.com/o/profile%2Fsamplepicture%2FEllipse%201.png?alt=media&token=0bb937e1-1235-4ad3-8e44-0aee7bf42fdc',
+      assistantList: [],
+      emergencyList: [],
+      chatList: [],
+   },
+];
