da49969fac1727233a2d71ffbebb0160d5acf7ef
Grapeka <chotwilaiwan@gmail.com>
2022-12-11 20:55:33 +0700

    style: add comments

--- src/__tests__/chat/chat.test.js
+++ src/__tests__/chat/chat.test.js
@@ -1,24 +1,26 @@
 import { getAllUserChat } from '../../utils/dbs/AuthDataOperator';
 
-import { messages } from '../mocks/chatData';
+import { messages } from '../../mocks/chat';
+import { realUser, fakeUser } from '../../mocks/user';
 
 import sortMessage from '../../utils/Message/SortMessage';
 import filterMessage from '../../utils/Message/FilterMessage';
 
 // get a user chat(s)
 test('get user chat. Found', async () => {
-   const userChat = await getAllUserChat('OxMqlmqrhzWzmoMscd3FeauPIw72');
+   const userChat = await getAllUserChat(realUser.uid);
    expect(userChat.length >= 1).toBe(true);
 });
-
-test('get user chat. Not found', () => {
-   const userChat = getAllUserChat('dummy');
-   expect(userChat.length >= 1).toBe(false);
+test('get user chat. Not found', async () => {
+   const userChat = await getAllUserChat(fakeUser.uid);
+   expect(userChat == null).toBe(true);
 });
 
 //Test the sortMessage function
 test('sort messages correctly', () => {
    const sortedMessages = sortMessage(messages);
+
+   // Check if the array is sorted
    expect(sortedMessages[0].timestamp.seconds).toBeLessThanOrEqual(
       sortedMessages[1].timestamp.seconds
    );
@@ -31,5 +33,6 @@ test('sort messages correctly', () => {
 test('filter messages correctly', () => {
    const filteredMessages = filterMessage(messages, 1, 2);
 
+   // Check if the array is filtered by counting the number of messages
    expect(filteredMessages.length).toBe(3);
 });
